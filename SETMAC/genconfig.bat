8b31b59d2ff255abac1e83ba63c8fb6b2e3f97ab
Daniel <Daniel@Vimes>
2012-09-22 14:48:18 +0100

    Initial commit.

--- SETMAC/genconfig.bat
+++
@@ -1,3 +0,0 @@
-del src\uk\ac\ed\inf\setmac\config\v1\*.java
-"C:\Program Files\Java\jdk1.6.0_29\bin\xjc.exe" -d src\ -mark-generated src\uk\ac\ed\inf\setmac\config\v1\config.xsd
-rmdir /S /Q src\uk\ac\ed\inf\ace
